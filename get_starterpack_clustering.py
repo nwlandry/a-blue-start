8a1da6aaa8b047a999fb61644c736b7879199398
Nicholas Landry <nicholas.landry.91@gmail.com>
2025-04-10 17:19:28 -0400

    updates

--- get_starterpack_clustering.py
+++ get_starterpack_clustering.py
@@ -1,14 +1,14 @@
-import pandas as pd
-import xgi
-
-H = xgi.read_hif("data/deidentified_starterpack_hif.json")
-
 import gc
+import json
 from itertools import combinations
 
 import igraph as ig
 import leidenalg
+import xgi
 from tqdm import tqdm
+
+H = xgi.read_hif("data/deidentified_starterpack_hif.json")
+xgi.largest_connected_hypergraph(H, in_place=True)
 
 print("Mapping nodes to unique integers...", flush=True)
 node_to_int = {node_id: idx for idx, node_id in enumerate(H.nodes)}
@@ -23,8 +23,8 @@ print("Constructing the edge list for the clique expansion...", flush=True)
 for edge in tqdm(H.edges, desc="Processing hyperedges", unit="edges"):
     nodes = [node_to_int[node_id] for node_id in H.edges.members(edge)]
     if len(nodes) >= 2:
-        for u, v in combinations(nodes, 2):
-            clique_edges_set.add((min(u, v), max(u, v)))
+        for u, v in combinations(sorted(nodes), 2):
+            clique_edges_set.add((u, v))
 
 print(
     f"Total number of edges in the clique expansion: {len(clique_edges_set)}",
@@ -32,15 +32,11 @@ print(
 )
 
 print("Creating the igraph Graph...", flush=True)
-edge_list = list(clique_edges_set)
-del clique_edges_set
-gc.collect()
-
 G_ig = ig.Graph()
 G_ig.add_vertices(num_nodes)
-G_ig.add_edges(edge_list)
+G_ig.add_edges(clique_edges_set)
 
-del edge_list
+del clique_edges_set
 gc.collect()
 
 print("Running the Leiden algorithm for community detection...", flush=True)
@@ -50,31 +46,12 @@ del G_ig
 gc.collect()
 
 print("Mapping nodes to clusters...", flush=True)
-node_to_cluster = {idx: cluster for idx, cluster in enumerate(partition.membership)}
+node_labels = {
+    str(int_to_node[idx]): cluster for idx, cluster in enumerate(partition.membership)
+}
 
 del partition
 gc.collect()
 
-print("calculating uncut hyperedges per size...", flush=True)
-counts_per_size = {}
-not_cut_counts_per_size = {}
-
-for edge in tqdm(H.edges, desc="Evaluating hyperedges", unit="edges"):
-    nodes = [node_to_int[node_id] for node_id in H.edges.members(edge)]
-    clusters = set(node_to_cluster[node] for node in nodes)
-    size = len(nodes)
-    counts_per_size[size] = counts_per_size.get(size, 0) + 1
-    if len(clusters) == 1:
-        not_cut_counts_per_size[size] = not_cut_counts_per_size.get(size, 0) + 1
-
-del node_to_int
-del int_to_node
-gc.collect()
-
-sizes = sorted(counts_per_size.keys())
-fractions = [
-    not_cut_counts_per_size.get(size, 0) / counts_per_size[size] for size in sizes
-]
-df = pd.DataFrame({"size": sizes[1:], "fraction": fractions[1:]})
-
-df.to_csv("data/starterpack_clustering.csv.gz", index=False)
+with open("data/node_labels.json", "w") as f:
+    f.write(json.dumps(node_labels))
