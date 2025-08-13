46a8e0e11703cf6f66ecb2dc98692ec2685bea67
Nicholas Landry <nicholas.landry.91@gmail.com>
2025-08-13 17:37:02 -0400

    updated analysis code

--- get_network_stats.py
+++ get_network_stats.py
@@ -1,23 +1,28 @@
 from collections import defaultdict
+from os.path import join
 
 import numpy as np
-import pandas as pd
+import polars as pl
 from tarjan import tarjan
 
-df = pd.read_csv("data/deidentified_follows_edgelist.csv", header=None)
+base_dir = "/Users/yyu8dx/Library/CloudStorage/OneDrive-UniversityofVirginia/Research/bluesky/SOMAR"
+
+df = pl.read_csv(
+    join(base_dir, "deidentified_follows_edgelist.csv"),
+    new_columns=["from", "to", "date_followed"],
+    has_header=False,
+)
 print("Loaded edgelist!", flush=True)
-n_nodes = len(set(df[0].unique()).union(df[1].unique()))
 
-out_degree = df.groupby(df[0]).agg("count")
-in_degree = df.groupby(df[1]).agg("count")
+out_degree = df["from"].value_counts()["count"].to_numpy()
+in_degree = df["to"].value_counts()["count"].to_numpy()
+follow_volume = df["date_followed"].value_counts().to_numpy()
+
 print("Computed degrees!", flush=True)
-
-source = df[0].to_numpy()
-target = df[1].to_numpy()
 
 gd = defaultdict(list)
 gu = defaultdict(list)
-for i, j in zip(source, target):
+for i, j, _ in df.iter_rows():
     gd[i].append(j)
     gu[i].append(j)
     gu[j].append(i)
@@ -34,5 +39,6 @@ print("Weakly connected component sizes computed!", flush=True)
 
 np.savetxt("data/follows_sccs.csv.gz", sccs, fmt="%d")
 np.savetxt("data/follows_wccs.csv.gz", wccs, fmt="%d")
+np.savetxt("data/follows_volume.csv", follow_volume, fmt=["%s", "%d"], delimiter=",")
 np.savetxt("data/follows_in-degree.csv.gz", in_degree, fmt="%d")
 np.savetxt("data/follows_out-degree.csv.gz", out_degree, fmt="%d")
